f82c6302fe4fcf48f3d9f8aa7ae60e0315eb2873
Judd Cohen <jcohen@juddnet.com>
2013-12-03 12:21:17 -0800

    Smoothly move the cube forward and back to demo GameObject movement

--- cs315_hwk5_webgl/Breakout.js
+++ cs315_hwk5_webgl/Breakout.js
@@ -4,9 +4,14 @@ var breakout;
 
 
 function Breakout() {
+	// GameObjects:
 	this.paddle = null;
 	this.blocks = [];
-	this.timer = 0;
+
+	// Timers:
+	this.rotTimer = 0;
+	this.moveTimer = 0;
+	this.dirFlip = true;
 
 
 	this.init = function() {
@@ -20,9 +25,27 @@ function Breakout() {
 
 
 	this.update = function(timeSinceLastFrame) {
-		this.timer += timeSinceLastFrame;
-		var angleInDegrees = (this.timer * 20) % 360.0;
+		//
+		// calculate smooth rotation
+		//
+		this.rotTimer += timeSinceLastFrame;
+		var angleInDegrees = (this.rotTimer * 20) % 360.0;
 		this.paddle.rotation[1] = angleInDegrees;
+
+		//
+		// calculate movement
+		//
+		if (this.moveTimer > 1.0) {
+			this.moveTimer = 0;
+			this.dirFlip = !this.dirFlip; // toggle depth direction
+		}
+		else
+			this.moveTimer += timeSinceLastFrame;
+
+		if (this.dirFlip == true)
+			vec3.lerp(this.paddle.position, [0, 0, 0], [0, 0, -2], (-this.moveTimer) + 1.0);
+		else
+			vec3.lerp(this.paddle.position, [0, 0, 0], [0, 0, -2], this.moveTimer);
 	}
 }
 
