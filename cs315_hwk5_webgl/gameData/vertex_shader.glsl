5aa132088542a66f9a8e1ad2b83ac999898109cc
paulmakl <paul@paulnmakl.com>
2013-12-18 01:42:28 -0800

    put the old shader back

--- cs315_hwk5_webgl/gameData/vertex_shader.glsl
+++ cs315_hwk5_webgl/gameData/vertex_shader.glsl
@@ -1,16 +1,27 @@
-uniform mat4 uMVMatrix; 	
-uniform mat4 uMVPMatrix;	
-attribute vec4 aPosition;	
-attribute vec3 aNormal;		
-attribute vec4 aColor;		
-varying vec4 vColor; 		
-vec3 lightPos = vec3(0.0,0.0,3.0); //the position of the light
+uniform mat4 uMVMatrix;    // A constant representing the modelview matrix. Used for calculating lights/shading
+uniform mat4 uMVPMatrix;   // A constant representing the combined modelview/projection matrix. We use this for positioning
+
+uniform vec3 uLightPos;    // light position
+
+attribute vec4 aPosition;  // Per-vertex position information we will pass in
+attribute vec3 aNormal;    // Per-vertex normal information we will pass in.
+attribute vec4 aColor;     // Per-vertex color information we will pass in.
+
+// all output variables:
+varying vec4 oPosition;
+varying vec4 oColor;        // the ultimate color of the vertex
+varying vec3 oNormal;
+varying vec3 oLightPos;
+
+
 void main() {
-  vec3 modelViewVertex = vec3(uMVMatrix * aPosition); 			
-  vec3 modelViewNormal = vec3(uMVMatrix * vec4(aNormal, 0.0));
-  vec3 lightVector = normalize(lightPos - modelViewVertex);		
-  float diffuse = max(dot(modelViewNormal, lightVector), 0.1);	
-  vColor = aColor * diffuse; 									
-  gl_PointSize = 3.0;	//for drawing points
-  gl_Position = uMVPMatrix * aPosition; position.
+        // precalculate the world space position
+        vec4 pos = uMVPMatrix * aPosition;
+        oPosition = pos;
+        gl_Position = pos;
+
+        // pass through the normal, color, and light position to the fragment shader
+        oNormal = aNormal;
+        oColor = aColor;
+        oLightPos = uLightPos;
 }
